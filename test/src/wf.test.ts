5d2be4aede8c0ff7f37fe273f7096192185eda86
Gleb Zakharov <snyssfx@posteo.net>
2024-09-30 12:38:05 +0200

    workflow: add test

--- test/src/wf.test.ts
+++ test/src/wf.test.ts
@@ -1,24 +1,3 @@
-// import {
-//   BlockArgs,
-//   platforma,
-// } from '@pl-open/my-org.block-boilerplate.model';
-// import { awaitStableState, blockTest } from '@pl-open/sdk-test';
-// import { blockSpec as myBlockSpec } from 'this-block';
-// import { InferBlockState, wrapOutputs } from '@pl-open/sdk-ui';
-
-// blockTest(
-//   'preset content',
-//   { timeout: 30000 },
-//   async ({ rawPrj: project, ml, helpers, expect }) => {
-//     const blockId = await project.addBlock('Block', myBlockSpec);
-//     await project.setBlockArgs(blockId, {
-//       argument: 'World!!',
-//     } satisfies BlockArgs);
-//     const stableState = (await awaitStableState(
-//       project.getBlockState(blockId),
-//       250000
-//     )) as InferBlockState<typeof platforma>;
-//     const output = wrapOutputs(stableState.outputs).transformedArg;
-//     expect(output).toEqual('Hello World!!!');
-//   }
-// );
+/*
+  There are no tests yet, create them via blockTest from @pl-open/sdk-test' function.
+*/
